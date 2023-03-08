a67095682859536ffa3864713ffe2f9d5a8fe3ec
Andrew <andrewthiser@gmail.com>
2023-03-08 12:22:48 -0500

    Only have to select the captions file once

--- captions.py
+++ captions.py
@@ -1,23 +1,33 @@
-def translate_captions(source_lang, dest_lang):
+import tkinter as tk
+from tkinter import filedialog, messagebox
+def translate_captions(caption_location, source_lang, dest_lang):
     try:
         import webvtt
     except ModuleNotFoundError:
         print("Please install the 'webvtt' library.")
+    try:
+        import googletrans
+    except ModuleNotFoundError:
+        print("Please install the 'googletrans' library.")
     from googletrans import Translator
-
-    captions = webvtt.read('C:\\Users\\andre\Downloads\\captions.vtt')
+    captions = webvtt.read(caption_location)
     translator = Translator()
     translated_captions = []
-
     for caption in captions:
         translated_text = translator.translate(caption.text, src=source_lang, dest=dest_lang).text
         translated_captions.append(translated_text)
         caption.text = translated_text
-
-    captions.save(f'C:\\Users\\andre\\Downloads\\translated_captions_{dest_lang}.vtt')
-
+    captions.save(f"{caption_location[:-4]}_{dest_lang}.vtt")
     return translated_captions
 
-translated_captions_cn = translate_captions('en', 'zh-CN')
-translated_captions_es = translate_captions('en', 'es')
-translated_captions_es = translate_captions('en', 'pt')
+root = tk.Tk()
+root.withdraw()
+caption_location = filedialog.askopenfilename(title='Select captions file', filetypes=[('VTT Files','*.vtt')])
+if not caption_location:
+    print('No file selected')
+    messagebox.showerror('Error', 'No file selected')
+    raise SystemExit
+
+translated_captions_cn = translate_captions(caption_location, 'en', 'zh-CN')
+translated_captions_es = translate_captions(caption_location, 'en', 'es')
+translated_captions_pt = translate_captions(caption_location, 'en', 'pt')
