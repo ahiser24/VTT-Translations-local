53a7d455ad47e3cdde221d0e91659e62bab026dd
Andrew <65128664+ahiser24@users.noreply.github.com>
2023-11-07 13:50:20 -0500

    Update captions.py
    Updated to work with new VTT files which include meeting ID's and a "-" or "/" on a line.

--- captions.py
+++ captions.py
@@ -1,5 +1,8 @@
+import os
+import time
 import tkinter as tk
 from tkinter import filedialog, messagebox
+
 def translate_captions(caption_location, source_lang, dest_lang):
     try:
         import webvtt
@@ -10,15 +13,33 @@ def translate_captions(caption_location, source_lang, dest_lang):
     except ModuleNotFoundError:
         print("Please install the 'googletrans' library.")
     from googletrans import Translator
-    captions = webvtt.read(caption_location)
+    import re  # Regular expression library
+
+    # Read the file and ignore lines with meeting ID
+    with open(caption_location, 'r') as f:
+        lines = f.readlines()
+    lines = [line for line in lines if not re.search(r'[-/]\d+$', line.strip())]
+
+    # Write the cleaned lines to a new file
+    temp_location = caption_location[:-4] + "_temp.vtt"
+    with open(temp_location, 'w') as f:
+        f.writelines(lines)
+
+    captions = webvtt.read(temp_location)
     translator = Translator()
     translated_captions = []
     for caption in captions:
         translated_text = translator.translate(caption.text, src=source_lang, dest=dest_lang).text
         translated_captions.append(translated_text)
         caption.text = translated_text
+        time.sleep(10) # Add a delay of 10 seconds
     captions.save(f"{caption_location[:-4]}_{dest_lang}.vtt")
+
+    # Remove the temporary file
+    os.remove(temp_location)
+
     return translated_captions
+
 
 root = tk.Tk()
 root.withdraw()
